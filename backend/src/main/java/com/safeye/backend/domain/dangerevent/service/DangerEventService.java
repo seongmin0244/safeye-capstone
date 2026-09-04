c59aea2c92366674e031b54c78495b354062b1de
Seongmin Kang <seongmin0244@hanyang.ac.kr>
2026-09-04 20:59:43 +0900

    refactor: 가상 엣지 더미 이미지 경로 수정 및 로그 상세화

--- backend/src/main/java/com/safeye/backend/domain/dangerevent/service/DangerEventService.java
+++ backend/src/main/java/com/safeye/backend/domain/dangerevent/service/DangerEventService.java
@@ -70,7 +70,7 @@ public class DangerEventService {
       return;
     }
 
-    String mockFileUrl = "http://localhost:8080/uploads/mock/" + file.getName();
+    String mockFileUrl = "http://localhost:8080/uploads/mock/images/" + file.getName();
 
     DangerEvent dangerEvent = DangerEvent.createDangerEvent(
         workZone,
